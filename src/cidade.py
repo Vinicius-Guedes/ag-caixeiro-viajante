3b30bb8cc0342b23336162e7720c1195c5e8d13e
Samuel Mayer Rufino <samrufino13@gmail.com>
2022-09-16 15:52:22 -0300

    Gerador de cidades

---
+++ src/cidade.py
@@ -0,0 +1,11 @@
+import random
+
+class Cidade:
+    def __init__(self) -> None:
+        self.pontoX = 0
+        self.pontoY = 0
+        self.id = 0
+    
+    def setCoordenada(self):
+        self.pontoX = random.randint(1,100)
+        self.pontoY = random.randint(1,100)
---
+++ src/geradorCidades.py
@@ -0,0 +1,32 @@
+from gc import get_referents
+from cidade import Cidade
+import math
+
+
+class GeradorCidades:
+    def __init__(self, qtdeCidade) -> None:
+        self.qtdeCidade = qtdeCidade
+        self.cidades = list()
+        self.distancias = list()
+        self.criarCidades()
+        self.definirDistancias()
+
+    def criarCidades(self) -> None:
+        for id in range(0, self.qtdeCidade):
+            cidade = Cidade()
+            cidade.setCoordenada()
+            cidade.id = id
+            self.cidades.append(cidade)
+
+    def definirDistancias(self) -> None:
+        for cidade in self.cidades:
+            distancia = list()
+            for cidade2 in self.cidades:
+                aux1 = (cidade.pontoX - cidade2.pontoX) ** 2
+                aux2 = (cidade.pontoY - cidade2.pontoY) ** 2
+                distancia.append( int(math.sqrt(aux1 + aux2)) )
+            self.distancias.append(distancia)
+
+if __name__ == "__main__":
+    teste = GeradorCidades(10)
+    print(teste.distancias)
